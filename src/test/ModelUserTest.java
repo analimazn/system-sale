b606b91cb830d5121f9c6c4f0664bbf0574d4e12
mayaracsferreira <mayara_csf@hotmail.com>
2018-12-01 16:52:34 -0200

    Add test in setUP before execution

--- src/test/ModelUserTest.java
+++ src/test/ModelUserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import entities.User;
@@ -11,35 +12,34 @@ import enums.EnumUser;
 import models.modelEntities.ModelUser;
 
 class ModelUserTest {
-	User user;
-	ModelUser modeluser;
+	User user1;
+	User user2;
+	User user3;
 	
-	@Test
-	public void addUser() {
-		user = new User("teste1", "@123", EnumUser.Admin, "00001", "Teste1", "00000000000000");
-		ModelUser.addUser( user );
-		assertEquals(true, modeluser.validateUser(user));
+	@BeforeEach
+	void setUp() throws Exception {
+		user1 = new User("teste1", "@123", EnumUser.Admin, "00001", "Teste1", "00000000000000");
+		user2 = new User("teste2", "@123", EnumUser.Client, "00002", "Teste2", "00000000000000");
+		user3 = new User("teste3", "@123", EnumUser.Client, "00003", "Teste3", "00000000000000");
 	}
 	
 	@Test
-	public void removeUser() {	
-		user = new User("teste2", "@123", EnumUser.Client, "00002", "Teste2", "00000000000000");
-		ModelUser.addUser( user );
-		modeluser.removeUser("00002");		
-		assertEquals(false, modeluser.validateUser(user));		
+	public void addUser() {		
+		ModelUser.addUser( user1 );
+		assertEquals(true, ModelUser.validateUser(user1));
 	}
 	
 	@Test
-	public void findUser() {
-		user = new User("teste3", "@123", EnumUser.Client, "00003", "Teste3", "00000000000000");
-		ModelUser.addUser( user );
-		//assertEquals(user.getId(), ModelUser.findUser( user.getId() ).getId()  );		
+	public void removeUser() {			
+		ModelUser.addUser( user2 );
+		ModelUser.removeUser(user2.getId());		
+		assertEquals(false, ModelUser.validateUser(user2));		
 	}
 	
 	@Test
-	public void updateUser() {
+	public void findUser() {		
+		ModelUser.addUser( user3 );
+		//assertEquals(user3.getId(), ModelUser.findUser( user3.getId() ).getId()  );		
+	}
 		
-	}
-	
-	
 }
