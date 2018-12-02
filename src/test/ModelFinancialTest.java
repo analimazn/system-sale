fc8ca45cb2a69b6b3d78597f01c74d0b042c4489
mayaracsferreira <mayara_csf@hotmail.com>
2018-12-02 15:02:29 -0200

    Add financial test case

--- src/test/ModelFinancialTest.java
+++ src/test/ModelFinancialTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import entities.Financial;
+import models.modelEntities.ModelFinancial;
 
 
 class ModelFinancialTest {
@@ -18,13 +19,23 @@ class ModelFinancialTest {
 	protected void setUp() throws Exception {
 		financial1 = new Financial("2323", "111111111111111", "Bradesco");
 		financial2 = new Financial("2424", "22222222222222", "Itau" );
-		financial3 = new Financial("2424", "22222222222222", "Itau" );
-		
+		financial3 = new Financial("2424", "22222222222223", "Itau" );		
 	}
 	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void addFinancial() {		
+		ModelFinancial.addFinancial( financial1 );
+		assertEquals(true, ModelFinancial.validateFinancial(financial1));
 	}
+	
+	@Test
+	public void removeFinancial() {			
+		ModelFinancial.addFinancial( financial2 );
+		ModelFinancial.removeFinancial(financial2.getId());		
+		assertEquals(false, ModelFinancial.validateFinancial(financial2));		
+	}
+	
+	
+			
 
 }
