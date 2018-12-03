f0a88d2006100ad809a3f37e5b5311d5807f0aed
mayaracsferreira <mayara_csf@hotmail.com>
2018-12-02 22:22:37 -0200

    Add status search

--- src/models/modelSystemSale/ModelSale.java
+++ src/models/modelSystemSale/ModelSale.java
@@ -1,8 +1,10 @@
 package models.modelSystemSale;
 
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import systemSale.Sale;
+import enums.EnumStatus;
 
 public class ModelSale {
 	static List<Sale> list = new LinkedList<Sale>();
@@ -10,21 +12,21 @@ public class ModelSale {
 	public static Boolean validateSale(Sale obj) {
 		for(Sale sale : list) {
 		   if(sale.getId().equals(obj.getId())) {
-			   System.out.println("Leil√£o encontrado");
+			   System.out.println("Leil„o encontrado");
 			   return true;
 		    }
 		}
-		System.out.println("Leil√£o nƒÉo encontrado");
+		System.out.println("Leil„o n„o encontrado");
 		return false;
 	}
 	
 	public static void addSale(Sale obj) {
 		if(!validateSale(obj)) {
 			list.add(obj);
-			System.out.println("Leil√£o cadastrado com sucesso");
+			System.out.println("Leil„o cadastrado com sucesso");
 			System.out.println(obj.getSale());
 		} else {
-			System.out.println("Leil√£o j√° cadastrado");
+			System.out.println("Leil„o j· cadastrado");
 		}
 	}
 	
@@ -37,7 +39,7 @@ public class ModelSale {
 				return false;
 			})
 			.forEach(obj -> {
-				System.out.println("Leil√£o");
+				System.out.println("Leil„o");
 				System.out.println(obj.getSale());
 			});
 		return null;
@@ -45,36 +47,50 @@ public class ModelSale {
 	
 	public static void getListSale() {
 		if (list.size() > 0) {
-			System.out.println("Todos os leil√µes");
+			System.out.println("Todos os leilıes");
 			for(Sale obj: list) {
 				System.out.println(obj.getSale());
 			}
 		} else {
-			System.out.println("Sem Leil√µes");
+			System.out.println("Sem Leilıes");
 		}
 	}
 	
 	public static void updateSale(Sale change) {
 		for (Sale obj : list) {
 			if (obj.getId() == change.getId()) {
-				System.out.println("Atualizar Leil√£o");
+				System.out.println("Atualizar Leil„o");
 				System.out.println(obj.getSale());
 				list.set(list.indexOf(obj), change);
 			}
 		}
-		System.out.println("Leil√£o atualizado");
+		System.out.println("Leil„o atualizado");
 		System.out.println(change.getSale());
 	}
 	
 	public static void removeSale(String id) {
 		for (Sale obj : list) {
 			if (obj.getId() == id) {
-				System.out.println("Remover Leil√£o");
+				System.out.println("Remover Leil„o");
 				System.out.println(obj.getSale());
 				list.remove(obj);
 			} 
 		}		
 	}
 	
+	public static String getStatus(Sale sale) {		
+	LocalDate now = LocalDate.now();
+		
+		if((now.isAfter(sale.getOpeningDateBid()))&&(now.isBefore(sale.getProposalDateBid()))) {
+			return EnumStatus.Occurring.toString();
+		}
+		if(now.isAfter(sale.getProposalDateBid())) {
+			return EnumStatus.Finalized.toString();
+		}
+		else
+			return EnumStatus.Open.toString();
+	}
+		
+
 	
 }
